6d0814771a9db5c538bf6fb9eb38f1a9a6c6a84b
Jessica Seo <jessicaseo83@gmail.com>
2022-01-06 15:46:57 -0500

    Replace button with HeaderCartButton component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from 'react';
 import mealsImage from '../../assets/meals.jpeg';
 import styles from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={styles.header}>
         <h1>Yummy Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={styles['main-image']}>
         <img src={mealsImage} alt="delicious food"/>
