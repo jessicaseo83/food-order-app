5e043625fe963364bb505388974f85f430d2a5cd
Jessica Seo <jessicaseo83@gmail.com>
2022-01-07 01:05:11 -0500

    Replace with Card component and import MealItem component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
-import styles from './AvailableMeals.module.css'
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import styles from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
   {
@@ -28,12 +30,19 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
+  const mealsList = DUMMY_MEALS.map(meal => 
+    <MealItem 
+      key={meal.id} 
+      name={meal.name} 
+      description={meal.description} 
+      price={meal.price}
+    />
+  )
   return (
     <section className={styles.meals}>
-      <ul>
+      <Card>
         {mealsList}
-      </ul>
+      </Card>
     </section>
   )
 }
