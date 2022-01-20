31e28b5f6d3591b0dce1c62e3768e752c2b93bfc
Jessica Seo <jessicaseo83@gmail.com>
2022-01-20 16:38:40 -0500

    Add a Cart Context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartDisplay, setCartDisplay] = useState(false);
@@ -16,13 +17,13 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartDisplay && <Cart onClose={hideCartHander}/>}
       <Header onDisplayCart={displayCartHander}/>
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
